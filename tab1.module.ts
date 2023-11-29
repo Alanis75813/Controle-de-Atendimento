125d7d38220921d7fe81405684ad8a2e643b4228
Alanis75813 <136914767+Alanis75813@users.noreply.github.com>
2023-11-29 16:52:55 -0300

    Create tab1.module.ts

---
+++ tab1.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { IonicModule } from '@ionic/angular';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+
+import { Tab1Page } from './tab1.page';
+
+@NgModule({
+  imports: [
+    IonicModule,
+    CommonModule,
+    FormsModule,
+    RouterModule.forChild([{ path: '', component: Tab1Page }])
+  ],
+  declarations: [Tab1Page]
+})
+export class Tab1PageModule {}
